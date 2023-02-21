aa0c143248a0fd5bbfe03b30a8ec341587cce75c
Sami Sezgin <sezginsami@gmail.com>
2023-02-22 01:37:36 +0300

    Unit Tests and Abstract Test added.

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -12,9 +12,12 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.context.annotation.Scope;
 
 import javax.annotation.security.PermitAll;
 
+@org.springframework.stereotype.Component
+@Scope("prototype")
 @PermitAll
 @Route(value = "", layout = MainLayout.class)
 @PageTitle("Contacts | Vaadin CRM")
---
+++ src/test/java/com/example/application/it/elements/LoginViewElement.java
@@ -0,0 +1,34 @@
+package com.example.application.it.elements;
+
+import com.vaadin.flow.component.login.testbench.LoginFormElement;
+import com.vaadin.flow.component.orderedlayout.testbench.VerticalLayoutElement;
+import com.vaadin.testbench.annotations.Attribute;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+@Attribute(name = "class", contains = "login-view")
+public class LoginViewElement extends VerticalLayoutElement {
+
+    public boolean login(String username, String password) {
+        LoginFormElement form = $(LoginFormElement.class).first();
+        form.getUsernameField().setValue(username);
+        form.getPasswordField().setValue(password);
+        form.getSubmitButton().click();
+
+        // Return true if we end up on another page
+        return !$(LoginViewElement.class).onPage().exists();
+    }
+
+    @Test
+    public void loginAsValidUserSucceeds() {
+        LoginViewElement loginView = $(LoginViewElement.class).onPage().first();
+        Assert.assertTrue(loginView.login("user", "userpass"));
+    }
+
+    @Test
+    public void loginAsInvalidUserFails() {
+        LoginViewElement loginView = $(LoginViewElement.class).onPage().first();
+        Assert.assertFalse(loginView.login("user", "invalid"));
+    }
+
+}
